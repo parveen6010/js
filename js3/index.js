c0fa4dd0fde55f1c7fb897e8523970c40daeaf61
parveen6010 <parveenprajapati6010@gmail.com>
2022-11-06 19:54:36 +0530

    it

---
+++ js3/index.js
@@ -0,0 +1,20 @@
+const charval = document.getElementById("textarea");
+let totalCount = document.getElementById("total-conter");
+let remainingCount = document.getElementById("remaining-counter");
+
+let char = 0;
+const  updateCounter = () => {
+  userchar = charval.value.length;
+  totalCount.innerText = userchar;
+
+  remainingCount.innerText = 150-userchar;
+};
+
+charval.addEventListener('keyup' , () => updateCounter() );
+
+
+const copyText = () =>{
+  charval.select();  
+  charval.setSelectionRange(0,9999);
+  navigator.clipboard.writeText(charval.value);
+};  
